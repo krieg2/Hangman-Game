6b9edfae05232091016bd283f3d2507777f25bb2
krieg2 <aaronk1@yahoo.com>
2017-08-27 15:35:11 -0400

    update game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,8 @@
 var answer = chooseGameAnswer();
 var guessedLetters = [];
 var tries = 14;
+var gameCounter = 0;
+var wins = 0;
 createBoard(answer);
 
 
@@ -9,20 +11,46 @@ document.onkeyup = function(event) {
     var keyPressed = event.key;
 
     if(!guessedLetters.includes(keyPressed)){
-        guessedLetters.push(keyPressed);
-        tries--;
-        updateTries();
-    }
-    showGuesses(guessedLetters);
 
-    if(answer.includes(keyPressed)){
-        showLetter(keyPressed, answer);
+        guessedLetters.push(keyPressed);
+
+        if(answer.includes(keyPressed)){
+            showLetter(keyPressed, answer);
+            if(gameCounter===answer.length){
+            	updateLastAnswer(answer);
+            	resetGame(true);
+            }
+        } else{
+            tries--;
+            updateTries();
+            if(tries===0){
+            	resetGame(false);
+            }
+        }
+        showGuesses(guessedLetters);
     }
+
+ 
+}
+
+function resetGame(won){
+    if(won===true){
+        wins++;
+        updateWins();
+    }
+    tries = 14;
+    gameCounter = 0;
+    guessedLetters = [];
+    answer = chooseGameAnswer();
+    createBoard(answer);
+    showGuesses(guessedLetters);
 }
 
 function chooseGameAnswer(){
 
-    var islandList = ["sumatra", "tahiti", "bora bora", "bali", "maui", "boracay", "palawan", "jamaica"];
+    var islandList = ["sumatra", "tahiti", "bora bora",
+                      "bali", "maui", "boracay",
+                      "palawan", "jamaica"];
 
     var answer = "";
     var index = 0;
@@ -33,8 +61,8 @@ function chooseGameAnswer(){
     answer = islandList[index];
 
     return answer;
-
 }
+
 
 function showLetter(letter, answer){
 	var element;
@@ -42,6 +70,7 @@ function showLetter(letter, answer){
         if(answer.charAt(i)===letter){
             element = document.getElementById("letter_" + i);
             element.innerText = letter;
+            gameCounter++;
         }
     }
 }
@@ -59,14 +88,26 @@ function showGuesses(guessedLetters){
 function createBoard(answer){
     var gameBoard = document.getElementById("gameBoard");
 
+    gameBoard.innerHTML = "";
     for(var i=0; i<answer.length; i++){
-        gameBoard.innerHTML += "<div style=\"float:left;margin:20px;\" id=letter_" + i + "> _ </div>";
+        gameBoard.innerHTML += 
+          "<div style=\"float:left;margin:20px;\" id=letter_" + i + "> _ </div>";
     }
 
     updateTries();
 }
 
 function updateTries(){
-    var guesses = document.getElementById("tries");
-    guesses.innerText = tries;
+    var element = document.getElementById("tries");
+    element.innerText = tries;
+}
+
+function updateWins(){
+    var element = document.getElementById("wins");
+    element.innerText = wins;
+}
+
+function updateLastAnswer(answer){
+    var element = document.getElementById("lastAnswer");
+    element.innerText = answer;
 }
